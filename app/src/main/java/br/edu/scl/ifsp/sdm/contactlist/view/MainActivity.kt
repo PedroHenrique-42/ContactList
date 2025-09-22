e34ad4dcffa816fb9bbbc1c84ea052058102976e
Pedro Henrique <phlferezin03@gmail.com>
2025-09-22 17:23:59 -0300

    feat: implementando visualização com RecyclerView

--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
@@ -13,14 +13,13 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.edu.scl.ifsp.sdm.contactlist.R
-import br.edu.scl.ifsp.sdm.contactlist.adapter.ContactAdapter
 import br.edu.scl.ifsp.sdm.contactlist.adapter.ContactRvAdapter
 import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityMainBinding
 import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_CONTACT
 import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_VIEW_CONTACT
 import br.edu.scl.ifsp.sdm.contactlist.model.Contact
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnContactClickListener {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -30,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     // Adapter
     private val contactAdapter: ContactRvAdapter by lazy {
-        ContactRvAdapter(contactList)
+        ContactRvAdapter(contactList, this)
     }
 
     private lateinit var carl: ActivityResultLauncher<Intent>
@@ -48,7 +47,8 @@ class MainActivity : AppCompatActivity() {
                     val contact = result.data?.getParcelableExtra<Contact>(EXTRA_CONTACT)
                     contact?.also { newOrEditedContact ->
                         if (contactList.any { it.id == newOrEditedContact.id }) {
-                            val position = contactList.indexOfFirst { it.id == newOrEditedContact.id }
+                            val position =
+                                contactList.indexOfFirst { it.id == newOrEditedContact.id }
                             contactList[position] = newOrEditedContact
                         } else {
                             contactList.add(newOrEditedContact)
@@ -62,13 +62,6 @@ class MainActivity : AppCompatActivity() {
 
         amb.contactsRv.adapter = contactAdapter
         amb.contactsRv.layoutManager = LinearLayoutManager(this)
-
-//        amb.contactsLv.setOnItemClickListener { _, _, position, _ ->
-//            startActivity(Intent(this, ContactActivity::class.java).apply {
-//                putExtra(EXTRA_CONTACT, contactList[position])
-//                putExtra(EXTRA_VIEW_CONTACT, true)
-//            })
-//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -123,6 +116,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+    }
+
+    override fun onContactClick(position: Int) {
+        startActivity(Intent(this, ContactActivity::class.java).apply {
+            putExtra(EXTRA_CONTACT, contactList[position])
+            putExtra(EXTRA_VIEW_CONTACT, true)
+        })
     }
 
     private fun fillContacts() {
